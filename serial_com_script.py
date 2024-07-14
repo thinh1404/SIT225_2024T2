6980942869251e2290e0400284788347effad3c3
JimmyThinh1404 <141311632+thinh1404@users.noreply.github.com>
2024-07-14 12:49:43 +1000

    Add files via upload

---
+++ serial_com_script.py
@@ -0,0 +1,47 @@
+import random as rd
+import time
+import serial
+from datetime import datetime
+
+# Set the baud rate
+baud_rate = 9600
+
+# Create a serial port communication
+s = serial.Serial(port = "COM7", baudrate = baud_rate, timeout=20)
+
+while True:
+    # SENDING STAGE
+    # Random times blink of LED
+    timesBlink = rd.randint(1,5)
+    
+    # Get the sending timestamp
+    sendTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Send the data to Arduino
+    s.write(bytes(str(timesBlink),"utf-8"))
+    
+    print(f"{sendTime} Send >>> {timesBlink} blink times")
+    
+    
+    # RECEIVING STAGE
+    # Receive the sleep time from Arduino
+    sleepTime = s.readline().decode("utf-8").strip()
+    
+    # Receive timestamp
+    recvTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    print(f"{recvTime} Receive <<< {sleepTime} sleeping seconds")
+    
+    # Sleeping stage
+    sleepTime = int(sleepTime)
+    time.sleep(sleepTime)
+    
+    # Time stamp that python finishes sleeping
+    wakeupTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    print(f"{wakeupTime} wake up")
+    
+    print("=" * 10)
+    print()
+    
+    
