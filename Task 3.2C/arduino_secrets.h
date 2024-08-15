f3fe6bf8f5455cac8241f7975bcaf01d0632e7c0
JimmyThinh1404 <141311632+thinh1404@users.noreply.github.com>
2024-08-15 14:46:29 +1000

    Add files via upload

---
+++ Task 3.2C/arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "LongDauKec";
+#define SECRET_OPTIONAL_PASS "LolQuang6969"
---
+++ Task 3.2C/sketch_Task3.2C.ino
@@ -0,0 +1,107 @@
+#include "thingProperties.h"
+
+// Configure the PIN
+const int trigger = 2;
+const int echo = 3;
+
+
+int getUltrasonicDistance(){
+  // Function to retreive the distance reading of the ultrasonic sensor
+  long duration;
+
+
+  // Assure the trigger pin is LOW:
+  digitalWrite(trigger, LOW);
+  // Brief pause:
+  delayMicroseconds(5);
+
+  // Trigger the sensor by setting the trigger to HIGH:
+  digitalWrite(trigger, HIGH);
+  // Wait a moment before turning off the trigger:
+  delayMicroseconds(10);
+  // Turn off the trigger:
+  digitalWrite(trigger, LOW);
+
+  // Read the echo pin:
+  duration = pulseIn(echo, HIGH);
+  // Calculate the distance in centimeter (CM):
+  distance = duration * 0.034 / 2;
+
+  // Uncomment this line to return value in IN instead of CM:
+  //distance = distance * 0.3937008
+
+  // Return the distance read from the sensor:
+  return distance;
+}
+void setup() {
+  // Define inputs and outputs:
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+
+  // Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+  delay(1500); 
+
+  // Defined in thingProperties.h
+  initProperties();
+
+  // Connect to Arduino IoT Cloud
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  
+  /*
+     The following function allows you to obtain more information
+     related to the state of network and IoT Cloud connection and errors
+     the higher number the more granular information you’ll get.
+     The default is 0 (only errors).
+     Maximum is 4
+ */
+  setDebugMessageLevel(2);
+  ArduinoCloud.printDebugInfo();
+}
+
+void loop() {
+  ArduinoCloud.update();
+  // Your code here 
+  // Print the distance to the serial monitor:
+  distance = getUltrasonicDistance();
+  Serial.print("Distance: ");
+
+  if (distance < min_threshold || distance > max_threshold) // If the object is too near the sensor  or exceeds the maximum threshold, it will trigger the alarm.
+  {
+    triggered = true;
+  }
+  else {
+    triggered = false;
+  }
+  Serial.println(distance);
+
+  // Wait one second before continuing:
+  delay(1000);
+}
+
+/*
+  Since Distance is READ_WRITE variable, onDistanceChange() is
+  executed every time a new value is received from IoT Cloud.
+*/
+void onDistanceChange()  {
+  // Add your code here to act upon Distance change
+  Serial.println("--onDistanceChange");
+}
+void onTriggeredChange()
+{
+  Serial.print("--onTriggeredChange");
+}
+void onMaxThresholdChange()
+{
+  Serial.print("--onMaxThresholdChange");
+}
+void onMinThresholdChange()
+{
+  Serial.print("--onMinThresholdChange");
+}
+/*
+  Since RandomTemperature is READ_WRITE variable, onRandomTemperatureChange() is
+  executed every time a new value is received from IoT Cloud.
+*/
+
